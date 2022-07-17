f1249669870c1b424fdd40a6a9a605e6bfaf1ddc
akshatcoder-hash <akshatcoder@outlook.com>
2022-07-17 16:21:15 +0530

    Added Week-2 progress

---
+++ Week2-BuyMeACoffee-contracts/contracts/BuyMeACoffee.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract BuyMeACoffee {
+    // Event to emit when a Memo is created.
+    event NewMemo()
+
+    // Memo struct.
+    struct Memo
+}
